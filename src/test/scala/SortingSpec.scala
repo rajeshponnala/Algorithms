eac9bece49c81c14a179c522f5429eac9a590969
rajeshponnala <rajesh.ponnala@technoidentity.com>
2015-05-12 15:38:43 +0530

    Add tests for Selection Sort

--- src/test/scala/SortingSpec.scala
+++ src/test/scala/SortingSpec.scala
@@ -14,4 +14,32 @@ class SortingSpec extends FlatSpec with ShouldMatchers {
   "Selection Sort" should "return array in descending order" in {
     selectionSort(Array(-9,6,4,5,8,7,10),_>_) should equal(Array(10,8,7,6,5,4,-9))
   }
+
+  "Selection Sort" should "return negative integer array in ascending order" in {
+    selectionSort(Array(-5,-7,-8,-3,-9,-2),_<_) should equal(Array(-9,-8,-7,-5,-3,-2))
+  }
+
+  "Selection Sort" should "return negative integer array in descending order" in {
+    selectionSort(Array(-9,-5,-8,-3,-7,-2,-1),_>_) should equal(Array(-1,-2,-3,-5,-7,-8,-9))
+  }
+
+  "Selection Sort" should "return sorted  array(ascending) in same order" in {
+    selectionSort(Array(1,2,3,4,5,6,7,8,9,10),_<_) should equal(Array(1,2,3,4,5,6,7,8,9,10))
+  }
+
+  "Selection Sort" should "return sorted  array(descending) in same order" in {
+    selectionSort(Array(1,2,3,4,5,6,7,8,9,10),_>_) should equal(Array(10,9,8,7,6,5,4,3,2,1))
+  }
+
+  "Selection Sort" should "return reverse sorted  array in correct order(descending)" in {
+    selectionSort(Array(-10,-9,-8,-7,-6,-5,-4,-3,-2,-1),_>_) should equal(Array(-1,-2,-3,-4,-5,-6,-7,-8,-9,-10))
+  }
+
+  "Selection Sort" should  "return duplicates array in sorted order(descending)" in {
+    selectionSort(Array(5,6,7,8,5,9,2,2),_>_) should equal(Array(9,8,7,6,5,5,2,2))
+  }
+
+  "Selection Sort" should  "return  array in sorted order(descending)" in {
+    selectionSort((1 to 10000).toArray, _>_) should equal((1 to 10000).toArray.reverse)
+  }
 }
