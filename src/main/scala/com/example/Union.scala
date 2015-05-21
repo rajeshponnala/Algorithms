a710aa0da3ae0ecf103eee8282e495e68c5f2f12
rajeshponnala <rajesh.ponnala@technoidentity.com>
2015-05-21 18:50:01 +0530

    implement union using immutable map and Set

--- src/main/scala/com/example/Union.scala
+++ src/main/scala/com/example/Union.scala
@@ -4,6 +4,7 @@ case class Union() {
 
    val data = scala.collection.mutable.Map.empty[Int,scala.collection.mutable.Set[Int]]
    var data1 = Map.empty[Int,scala.collection.mutable.Set[Int]]
+   var data2 = Map.empty[Int,scala.collection.immutable.Set[Int]]
 
   def quickFind(p: Int,q: Int) = (data.get(p),data.get(q)) match {
      case (Some(x),Some(y)) =>  x == y
@@ -11,6 +12,11 @@ case class Union() {
   }
 
   def quickFind1(p: Int,q: Int) = (data1.get(p),data1.get(q)) match {
+     case (Some(x),Some(y)) =>  x == y
+     case _ => false
+  }
+
+  def quickFind2(p: Int,q: Int) = (data2.get(p),data2.get(q)) match {
      case (Some(x),Some(y)) =>  x == y
      case _ => false
    }
@@ -33,5 +39,15 @@ case class Union() {
         case (Some(x),Some(y)) => x ++ y
      }
      data1 = set.foldLeft(data1)((acc,s) => acc + (s -> set))
+  }
+
+  def quickUnion2(p: Int,q: Int): Unit = {
+     val set = (data2.get(p),data2.get(q)) match {
+        case (None,None) =>Set(p,q)
+        case (Some(x),None) => x + q
+        case (None,Some(x)) => x + p
+        case (Some(x),Some(y)) => x ++ y
+     }
+     data2 = set.foldLeft(data2)((acc,s) => acc + (s -> set))
    }
 }
--- src/test/scala/UnionSpecification.scala
+++ src/test/scala/UnionSpecification.scala
@@ -14,6 +14,11 @@ class UnionSpecification extends FlatSpec with ShouldMatchers {
     qu.quickUnion1(3,4)
     qu.quickUnion1(4,5)
     qu.quickUnion1(7,10)
+    qu.quickUnion2(1,2)
+    qu.quickUnion2(2,3)
+    qu.quickUnion2(3,4)
+    qu.quickUnion2(4,5)
+    qu.quickUnion2(7,10)
 
    "quick Union" should "return connected" in {
     qu.quickFind(3,4) should equal(true)
@@ -61,5 +66,28 @@ class UnionSpecification extends FlatSpec with ShouldMatchers {
 
   "point 1 and 7 of union 1" should "not connected" in {
    qu.quickFind1(1,7) should equal(false)
+  }
+  "quick Union of union 2" should "return connected" in {
+    qu.quickFind2(3,4) should equal(true)
+   }
+
+  "point (3,4) and (4,3) of union 2" should "be equal" in {
+    qu.quickFind2(3,4) should equal(qu.quickFind2(4,3))
+  }
+
+  "point 3 and 5 of union 2" should "return connected" in {
+     qu.quickFind2(3,5) should equal(true)
+  }
+
+  "point 1 and 3 of union 2" should "return  connected" in {
+    qu.quickFind2(1,3) should equal(true)
+  }
+
+  "point 1 and 5 of union 2" should "return  connected" in {
+    qu.quickFind2(1,3) should equal(true)
+  }
+
+  "point 1 and 7 of union 2" should "not connected" in {
+   qu.quickFind2(1,7) should equal(false)
   }
 }
