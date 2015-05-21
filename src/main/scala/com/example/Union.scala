dac222b860912957b150bd104fdb2a9f7fb806aa
rajeshponnala <rajesh.ponnala@technoidentity.com>
2015-05-21 18:35:25 +0530

    implement union using mutable map and mutable set

--- src/main/scala/com/example/Union.scala
+++ src/main/scala/com/example/Union.scala
@@ -1,18 +1,20 @@
 package com.example
 
-case class Union(n: Int) {
+case class Union() {
 
-  require(n > 0,"value should be greater than 0")
+   val data = scala.collection.mutable.Map.empty[Int,scala.collection.mutable.Set[Int]]
+   def quickFind(p: Int,q: Int) = (data.get(p),data.get(q)) match {
+     case (Some(x),Some(y)) =>  x == y
+     case _ => false
+   }
 
-   var  data = (for(x <- 0 to n-1) yield x).toVector
-
-  private def  quickFind(p : Int) = data(p)
-
-  def isConnected(p: Int,q: Int) = quickFind(p) == quickFind(q)
-
-  def quickUnion(p: Int,q: Int): Unit = {
-    val qv = quickFind(q)
-    val pv = quickFind(p)
-    data = if(pv == qv) data else data.map { e => if(e==pv) qv else e}
-  }
+   def quickUnion(p: Int,q: Int): Unit = {
+     val set = (data.get(p),data.get(q)) match {
+        case (None,None) => scala.collection.mutable.Set(p,q)
+        case (Some(x),None) => x+=q
+        case (None,Some(x)) => x+=p
+        case (Some(x),Some(y)) => x ++ y
+     }
+     set.foreach(s => data+=(s -> set))
+   }
 }
---
+++ src/test/scala/UnionSpecification.scala
@@ -0,0 +1,36 @@
+import org.scalatest._
+import org.scalatest.matchers.ShouldMatchers
+import com.example._
+
+class UnionSpecification extends FlatSpec with ShouldMatchers {
+    val qu =  new Union
+    qu.quickUnion(1,2)
+    qu.quickUnion(2,3)
+    qu.quickUnion(3,4)
+    qu.quickUnion(4,5)
+    qu.quickUnion(7,10)
+
+   "quick Union" should "return connected" in {
+    qu.quickFind(3,4) should equal(true)
+   }
+
+  "point (3,4) and (4,3)" should "be equal" in {
+    qu.quickFind(3,4) should equal(qu.quickFind(4,3))
+  }
+
+  "point 3 and 5" should "return connected" in {
+     qu.quickFind(3,5) should equal(true)
+  }
+
+  "point 1 and 3" should "return  connected" in {
+    qu.quickFind(1,3) should equal(true)
+  }
+
+  "point 1 and 5" should "return  connected" in {
+    qu.quickFind(1,3) should equal(true)
+  }
+
+  "point 1 and 7" should "not connected" in {
+   qu.quickFind(1,7) should equal(false)
+  }
+}
